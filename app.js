b5164ed63c3b0064f26d0ec2173c2cd1d959d455
kanishk1123 <apimails1@gmail.com>
2024-05-21 10:13:59 +0000

    updaet

--- app.js
+++ app.js
@@ -244,7 +244,7 @@ app.post("/user/addBookmark", async (req, res) => {
             return res.status(400).send({ message: "Missing required fields" });
         }
 
-        console.log(`Adding bookmark for user: ${email}`);
+        console.log(`Adding bookmark for user: ${req.body.email}`);
 
         const user = await usermodel.findOne({ email: req.body.email });
         if (!user) {
